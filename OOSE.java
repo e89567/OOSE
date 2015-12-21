f0bb20710b901f40d31da7411791993bed6fe00d
陳建佑 <e89567@gmail.com>
2015-12-21 22:36:27 +0800

    test

---
+++ OOSE.java
@@ -0,0 +1,9 @@
+class Question {
+   String description;
+   public Question(String desc) {
+      this.description = desc;
+   }
+   public void print() {
+      System.out.println(desc);
+   }      
+}

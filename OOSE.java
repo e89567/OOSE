1e36315d73c5ee75ba1c21db06d9989dc71ea314
e89567 <e89567@gmail.com>
2015-12-21 23:30:28 +0800

    Update OOSE.java

--- OOSE.java
+++ OOSE.java
@@ -1,10 +1,32 @@
-class Question {
+import java.util.ArrayList;  
+import java.util.Iterator;  
+public abstract class QuestionComponent {
+   publicQuestionItem(String desc) {}
+   public void print() {}      
+   public void getchild(int i) {}
+   //陳建佑 D0280197
+}
+
+class QuestionItem extends QuestionComponent{
    String description;
-   public Question(String desc) {
+   public QuestionItem(String desc) {
       this.description = desc;
    }
    public void print() {
       System.out.println(desc);
    }      
-   //陳建佑 D0280197
+  
+}
+class Question extends QuestionComponent{
+   String description;
+   private ArrayList  QuestionComponents;
+   public QuestionItem(String desc) {
+      this.description = desc;
+   }
+   public void print() {
+      System.out.println(desc);
+   } 
+   public void getchild(int i) {
+      return QuestionComponents.get(i)
+   }
 }
